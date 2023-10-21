225daeef1df551732c0556530dbd38ae412a547e
Jason Tajor <jasontajor0@gmail.com>
2023-10-21 15:38:15 +0800

    LoadingUpdate

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -5,9 +5,11 @@ function LoadingScreen() {
   return (
     <div className="loading-screen bg-primary">
       <div className="absolute w-8 h-8 border-4 border-t-4 border-green-500 rounded-md animate-spin"></div>
-      <h1 className='mt-32 text-sm font-semibold text-custom-blue typewriter'>
-        Please Wait...
-      </h1>
+      <div className='mt-32 text-sm font-semibold text-custom-blue'>
+        <h1 className="typewriter">
+          Please Wait...
+        </h1>
+      </div>
     </div>
   );
 }
